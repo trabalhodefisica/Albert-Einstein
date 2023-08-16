08f4f37bbcfbc31ec60af86e579aff9316ac05b6
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-16 19:16:39 -0300

    Update

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,3 @@
-const pagina = document.querySelector("#conteudo");
-const menu = pagina.querySelector("#menu");
-const botao_menu = menu.querySelector("#botao_menu")
-const altura_da_tela = window.screen.height;
-const largura_da_tela = window.screen.width;
 if(largura_da_tela <= 850){
     console.log(menu);
     console.log(largura_da_tela);
