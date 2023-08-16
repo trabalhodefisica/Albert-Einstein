a6d7d0933b3769e9724c0dabf9f07de719165fb5
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-16 19:43:29 -0300

    Update

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,32 @@
 if(largura_da_tela <= 850){
-    botao_menu.classList.remove("botao_nao-existe");
+    botao_menu.classList.remove("nao-existe");
     botao_menu.classList.add("botao_existe");
+    controla_links(botoes_link)
+    
 }
 
+botao_menu.addEventListener("click",()=>{
+    botao_menu.textContent = controla_botao(botao_menu)
+})
+
+
+
+
+function controla_botao(botao){
+let texto_do_botao = botao_menu.textContent
+let texto = "Abrir Menu"
+if(texto_do_botao == "Abrir Menu"){
+    texto = "Fechar Menu"
+}
+else{
+    texto = "Abrir Menu"
+}
+return texto;
+}
+
+function controla_links(botoes){
+    for(x = 0 ; x <= botoes.length ; x++){
+        botoes[x].classList.remove("pagina_link")
+        botoes[x].classList.add("nao-existe")
+    }
+}
--- js/variaveis.js
+++ js/variaveis.js
@@ -3,18 +3,5 @@ const menu = pagina.querySelector("#menu");
 const botao_menu = menu.querySelector("#botao_menu")
 const altura_da_tela = window.screen.height;
 const largura_da_tela = window.screen.width;
-botao_menu.addEventListener("click",()=>{
-        botao_menu.textContent = controla_botao(botao_menu)
-})
+const botoes_link = menu.querySelectorAll(".pagina_link_botoes");
 
-function controla_botao(botao){
-   let texto_do_botao = botao_menu.textContent
-   let texto = "Abrir Menu"
-   if(texto_do_botao == "Abrir Menu"){
-        texto = "Fechar Menu"
-   }
-   else{
-        texto = "Abrir Menu"
-   }
-   return texto;
-}
