21fceb004f77705bf297cf39c131b7221b19b23f
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-16 19:49:56 -0300

    Update

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,28 @@
 if(largura_da_tela <= 850){
     botao_menu.classList.remove("nao-existe");
     botao_menu.classList.add("botao_existe");
-    controla_links(botoes_link)
+    controla_links_mobile(botoes_link)
     
 }
+if(largura_da_tela > 850){
+    botao_menu.classList.remove("botao_existe");
+    botao_menu.classList.add("nao-existe");
+}
+
 
 botao_menu.addEventListener("click",()=>{
     botao_menu.textContent = controla_botao(botao_menu)
 })
 
 
+function controla_links(botoes,botao){
+    if(botao.textContent == "Abrir Menu"){
 
+    }
+    else{
+
+    }
+}
 
 function controla_botao(botao){
 let texto_do_botao = botao_menu.textContent
@@ -24,7 +36,7 @@ else{
 return texto;
 }
 
-function controla_links(botoes){
+function controla_links_mobile(botoes){
     for(x = 0 ; x <= botoes.length ; x++){
         botoes[x].classList.remove("pagina_link")
         botoes[x].classList.add("nao-existe")
