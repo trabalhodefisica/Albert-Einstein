e0c4283302c688737ca2718a2f7987ee2949137e
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-16 19:28:40 -0300

    Update

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,5 @@
 if(largura_da_tela <= 850){
-    console.log(menu);
-    console.log(largura_da_tela);
     botao_menu.classList.remove("botao_nao-existe");
     botao_menu.classList.add("botao_existe");
 }
+
--- js/variaveis.js
+++ js/variaveis.js
@@ -3,3 +3,18 @@ const menu = pagina.querySelector("#menu");
 const botao_menu = menu.querySelector("#botao_menu")
 const altura_da_tela = window.screen.height;
 const largura_da_tela = window.screen.width;
+botao_menu.addEventListener("click",()=>{
+        botao_menu.textContent = controla_botao(botao_menu)
+})
+
+function controla_botao(botao){
+   let texto_do_botao = botao_menu.textContent
+   let texto = "Abrir Menu"
+   if(texto_do_botao == "Abrir Menu"){
+        texto = "Fechar Menu"
+   }
+   else{
+        texto = "Abrir Menu"
+   }
+   return texto;
+}
