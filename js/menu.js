5ef989bd1d96ae1255281f7d53ad5ce0e6aafcbb
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-16 19:13:48 -0300

    uPDATE

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 const pagina = document.querySelector("#conteudo");
 const menu = pagina.querySelector("#menu");
+const botao_menu = menu.querySelector("#botao_menu")
 const altura_da_tela = window.screen.height;
 const largura_da_tela = window.screen.width;
 if(largura_da_tela <= 850){
-    console.log(menu)
+    console.log(menu);
+    console.log(largura_da_tela);
+    botao_menu.classList.remove("botao_nao-existe");
+    botao_menu.classList.add("botao_existe");
 }
