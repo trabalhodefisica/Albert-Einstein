42883fdf7c29955d3d7d4d67fd434850d517de30
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-08-17 15:09:14 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,56 +1,18 @@
 if(largura_da_tela <= 850){
     botao_menu.classList.remove("nao-existe");
     botao_menu.classList.add("botao_existe");
-    controla_links_mobile(botoes_link,"desaparecer")
-    
 }
 if(largura_da_tela > 850){
     botao_menu.classList.remove("botao_existe");
     botao_menu.classList.add("nao-existe");
 }
 
-
-
-
-botao_menu.addEventListener("click",()=>{
+botao_menu.addEventListener("click", ()=>{
+    if(botao_menu.textContent == "Abrir Menu"){
+        links.style.display = "block"
+    }
+    if(botao_menu.textContent == "Fechar Menu"){
+        links.style.display = "none"
+    }
     botao_menu.textContent = controla_botao(botao_menu)
-    //controla_links(botoes_link,botao_menu)
 })
-
-
-function controla_links(botoes,botao){
-    if(botao.textContent == "Abrir Menu"){
-        controla_links_mobile(botoes,"desaparecer")
-    }
-    else{
-        controla_links_mobile(botoes,"aparecer")
-    }
-    console.log(botao.textContent)
-}
-
-function controla_botao(botao){
-let texto_do_botao = botao_menu.textContent
-let texto = "Abrir Menu"
-if(texto_do_botao == "Abrir Menu"){
-    texto = "Fechar Menu"
-}
-else{
-    texto = "Abrir Menu"
-}
-return texto;
-}
-
-function controla_links_mobile(botoes,acao){
-    if(acao == "desaparecer"){
-        for(x = 0 ; x <= botoes.length ; x++){
-            botoes[x].classList.remove("pagina_link")
-            botoes[x].classList.add("nao-existe")
-        }
-    }
-    if(acao == "aparecer"){
-        for(x = 0 ; x <= botoes.length ; x++){
-            botoes[x].classList.remove("nao-existe")
-            botoes[x].classList.add("pagina_link")
-        }
-    }
-}
--- js/variaveis.js
+++ js/variaveis.js
@@ -3,5 +3,5 @@ const menu = pagina.querySelector("#menu");
 const botao_menu = menu.querySelector("#botao_menu")
 const altura_da_tela = window.screen.height;
 const largura_da_tela = window.screen.width;
-const botoes_link = menu.querySelectorAll(".pagina_link_botoes");
+const links = menu.querySelector("#links")
 
