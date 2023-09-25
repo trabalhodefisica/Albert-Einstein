c2e26babd2fa00403a9a6624d519524eaaf2906e
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-09-25 20:41:36 -0300

    Add files via upload

---
+++ js/paginas/menu-paginas.js
@@ -0,0 +1,23 @@
+for (let index = 0; index < paginas.length; index++) {
+    const element = paginas[index];
+    const pagina_verificador = `Albert Einstein/ ${element}`
+    const verifica_pagina = pagina_verificador == pagina_atual
+    let novolink = document.createElement("p")
+    novolink.classList.add("pagina_link")
+    novolink.classList.add("pagina_link_botoes")
+    if(paginas[index] == "Início"){
+        novolink.innerHTML = `<a href="../index.html"
+        class="link_da_pagina">${element}</a>`
+    }else{
+        novolink.innerHTML = `<a href="../paginas/${element}.html"
+        class="link_da_pagina">${element}</a>`
+    }
+    
+    if(!verifica_pagina){
+        links_menu.appendChild(novolink)
+        
+    }else{
+        document.title = pagina_verificador
+    }
+    
+}
---
+++ js/paginas/paginas.js
@@ -0,0 +1,3 @@
+const paginas = ["Início","Biografia","Efeito Fotoelétrico","Legado","Movimento Browniano","Prêmio Nobel","Teoria da Relatividade Geral","Teoria da Relatividade Restrita"]
+let pagina_atual = document.title
+let links_menu = document.querySelector("#menu");
