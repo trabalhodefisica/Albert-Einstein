362fb42dcd8467f68932eeacec47d65eb29f41b6
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-09-27 16:19:24 -0300

    Update Update

--- js/paginas/paginas.js
+++
@@ -1,3 +0,0 @@
-const paginas = ["Início","Biografia","Efeito Fotoelétrico","Legado","Movimento Browniano","Prêmio Nobel","Teoria da Relatividade Geral","Teoria da Relatividade Restrita"]
-let pagina_atual = document.title
-let links_menu = document.querySelector("#menu");
