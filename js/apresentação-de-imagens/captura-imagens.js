d4333f1b62db50c54b02b95ba302ce0381690731
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-10-04 15:53:52 -0300

    Limpeza

--- js/apresentação-de-imagens/captura-imagens.js
+++
@@ -1,6 +0,0 @@
-const imagens = document.querySelector(".imagens");
-const imagem = document.querySelector(".image")
-const avança = document.querySelector("#avança-imagem");
-const volta = document.querySelector("#volta-imagem")
-const listaImagens_index = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg"]
-
