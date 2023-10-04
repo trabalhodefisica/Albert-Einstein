fb1afeaa660da1abef1a32349d265ddf5d1bebb8
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-10-04 15:55:47 -0300

    Add files via upload

---
+++ js/paginas/paginas.js
@@ -0,0 +1,2 @@
+const paginas = ["Início","Biografia","Efeito Fotoelétrico","Legado","Movimento Browniano","Prêmio Nobel","Teoria da Relatividade Geral","Teoria da Relatividade Restrita"]
+let pagina_atual = document.title
