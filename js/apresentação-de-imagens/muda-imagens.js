5a48e01a4020404ba4cd367e39edc1bc20858b46
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-10-08 17:10:33 -0300

    Update muda-imagens.js

--- js/apresentação-de-imagens/muda-imagens.js
+++ js/apresentação-de-imagens/muda-imagens.js
@@ -20,6 +20,27 @@ avança.addEventListener("click", (event) => {
 
 
 })
+document.addEventListener('keydown', function (event) {
+
+    const key = event.key;
+    if (key == "ArrowLeft") {
+        indice = alteraImagem_Indice("volta", indice);
+        url = alteraImagem_Url(listaImagens_index, indice, document.title);
+        imagem.src = url;
+    }
+    if (key == "ArrowRight") {
+        indice = alteraImagem_Indice("avança", indice);
+        url = alteraImagem_Url(listaImagens_index, indice, document.title);
+        imagem.src = url;
+        imagem.onerror = function () {
+            indice--;
+            url = alteraImagem_Url(listaImagens_index, indice, document.title);
+            imagem.src = url
+        }
+    }
+
+
+});
 
 function alteraImagem_Indice(acao, indice) {
     if (acao == "avança") {
