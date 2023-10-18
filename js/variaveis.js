de830b5ff557511605b536ab3b518a093861c391
trabalhodefisica <142044200+trabalhodefisica@users.noreply.github.com>
2023-10-18 15:10:11 -0300

    Limpeza

--- js/variaveis.js
+++
@@ -1,7 +0,0 @@
-const pagina = document.querySelector("#conteudo");
-const menu = pagina.querySelector("#menu");
-const botao_menu = menu.querySelector("#botao_menu")
-const altura_da_tela = window.screen.height;
-const largura_da_tela = window.screen.width;
-const links = menu.querySelector("#links")
-
